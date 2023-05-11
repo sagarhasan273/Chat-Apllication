257bdcd2c2317afbb60f97a2c02f0f2379a3eedd
sagarhasan273 <sagarhasan273@gmail.com>
2023-05-11 09:49:28 +0600

    login and registration designed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import Register from './pages/Register';
-import Login from './pages/Login';
+import Home from './pages/Home';
 import './style.scss';
 
 export default function App() {
     return (
       <div>
-        <Login />
+        <Home />
       </div>
     );
 }
